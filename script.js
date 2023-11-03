cee7aa70cbcf69252460b5861f091217b5ee6d81
Diego Carmona <diegocarmona@my.whccd.edu>
2023-11-03 01:47:22 -0700

    added greeting, added pi const, added min process

---
+++ script.js
@@ -0,0 +1,10 @@
+let myGreeting = "Hello, Good  Morning I am Diego";
+console.log(myGreeting)
+
+const pi = 3.14;
+console.log(pi)
+let pi = 234;
+console.log(pi)
+
+let myNumber = 6;
+console.log(Math.min(myNumber, pi))
